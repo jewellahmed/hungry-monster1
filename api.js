54b7f64bddc3ef59c8b65220e20066808ad45591
Saifullah Monsur Jewel <jewellahmed97@gmail.com>
2021-02-08 14:59:37 +0600

    second-commit

--- api.js
+++ api.js
@@ -1,34 +1,87 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
 .then(res => res.json())
 .then(data => displayMeals(data));
+
+    
 
  const displayMeals = meals => {
 
         const mealsDiv  = document.getElementById('meals');
-        meals.meals.forEach(meal => {
-
-            //const mealName = meal.strMeal;
+        meals.meals.forEach( meal => {
+            const inputMeal = document.getElementById('input');
+            const mealName = inputMeal.placeholder;
+            console.log(mealName);
+            if(mealName === meal.strMeal){
+            
            
             const mealDiv = document.createElement('div');     
      
          mealDiv.className = 'meal';
          const mealInfo = `
                
+               <img src="${meal.strMealThumb}">
+
               <h3>${meal.strMeal}</h3>
-              <button onclick="displayDetail('${mealName}')">Detail</button>
+              <button onclick="displayDetail('${meal.strMeal}')">Detail</button>
+
          
-         `  
+         `
            mealDiv.innerHTML = mealInfo;
      
            mealsDiv.appendChild(mealDiv);
-        });
-      
+        }
+        else{
+            console.log('error');
+        }
+    });
+
 
     }
-
+   
     const displayDetail = strMeal => {
-        const url =` https://www.themealdb.com/api/json/v1/1/search.php?/strMeal/${strMeal}`
-        console.log(url);
+        
+        const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${strMeal}`
+        
+        fetch(url)
+        console.log(url)
+        .then(res => res.json()) 
+        .then(data => renderMealInfo(data));
+       // console.log(data);
+       
+
+    }
+    
+        
+    const renderMealInfo = meal => {
+       //console.log(meal);
+      // console.log(strMeal);
+      //console.log(strMeal);
+
+        const mealDiv = document.getElementById('mealDetail');
+        //console.log(strMeal);
+
+
+        //console.log(mealDetail)
+        mealDiv.innerHTML = `
+        
+           <h3>${meal.strMeal}</h3>
+           <img src="${meal.strMealThumb}">
+        
+        `
 
     }
 
